03e02274e66529f1579ebdb342318ac0c8b2ce9d
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-10-06 21:45:54 +0700

    feat: Add Calendar

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -3,14 +3,19 @@ import { Helmet } from "react-helmet"
 import HeaderContainer from "../Header"
 import MainContainer from "./MainContainer"
 
-const HomePage = ({ t, isSidebarExpanded}) => (
+const HomePage = ({ t, isSidebarExpanded, sliderRef, goToCurrentMonth }) => (
   <div className="home-page h-screen w-full flex flex-col animate__animated animate__fadeIn">
     <Helmet>
       <title>{t('app_name') + ' | ' + t('pages.0')}</title>
       <meta name="description" content={t('app_desc')} />
     </Helmet>
     <HeaderContainer />
-    <MainContainer t={t} isSidebarExpanded={isSidebarExpanded} />
+    <MainContainer
+      t={t}
+      isSidebarExpanded={isSidebarExpanded}
+      sliderRef={sliderRef}
+      goToCurrentMonth={goToCurrentMonth}
+    />
   </div>
 )
 
