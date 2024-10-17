0c3e89235112c181b6c079e54d18b0c2cbeb0b85
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-10-17 19:47:36 +0700

    feat: Increase Hijri Date Calendar Accuracy

--- src/utils/data.js
+++ src/utils/data.js
@@ -32,87 +32,140 @@ const pages = () => [
   }
 ]
 
-const isMabimbsCompliant = (latitude, longitude, elevation, newMoonDate) => {
+const calculateNewMoon = (startDate, latitude, longitude, elevation, criteria) => {
   const observer = new Observer(latitude, longitude, elevation)
-  const sunset = SearchRiseSet(Body.Sun, observer, -1, newMoonDate, 30, elevation)
-  const newMoonTime = new AstroTime(newMoonDate)
-  const sunsetTime = new AstroTime(sunset.date)
-  const moonEquator = Equator(Body.Moon, newMoonTime, observer, true, true)
-  const moonHorizon = Horizon(sunsetTime, observer, moonEquator.ra, moonEquator.dec, 'normal')
-  const elongation = AngleFromSun(Body.Moon, newMoonTime)
-  return moonHorizon.altitude > 3 && elongation < 6.4
-}
-
-const isKHGTCompliant = (latitude, longitude, elevation, newMoonDate) => {
-  const observer = new Observer(latitude, longitude, elevation)
-  const sunset = SearchRiseSet(Body.Sun, observer, -1, newMoonDate, 30, elevation)
-  const newMoonTime = new AstroTime(newMoonDate)
-  return newMoonTime.tt < sunset.tt
-}
-
-const getHijriYear = newMoonDate => new Date(newMoonDate).getFullYear() - 622
-
-const getHijriMonth = daysSinceNewMoon => daysSinceNewMoon < 29 ? 1 : 2
-
-const calculateNextHijriDate = (newMoonDate, latitude, longitude, elevation, criteria) => {
-  const newMoonTime = new AstroTime(newMoonDate)
-  const nextNewMoon = SearchMoonPhase(0, newMoonTime, 30)
-  const nextNewMoonDate = new Date(nextNewMoon.tt * 86400000)
-  return calculateHijriDate(nextNewMoonDate, latitude, longitude, elevation, criteria)
-}
-
-const calculateHijriDate = (gregorianDate, latitude, longitude, elevation, criteria) => {
-  const astroTime = new AstroTime(gregorianDate)
-  const newMoonEvent = SearchMoonPhase(0, astroTime, -30)
-  const newMoonDate = new Date(newMoonEvent.tt * 86400000 - 68.184 * 3600000)
+  let date = startDate
+  let newMoonDate
+  let newMoon
+  let moonElongation
+  let dateInNewMoon
+  let sunset
+  let moonEquator
+  let moonHorizon
   if (criteria === '0') {
-    if (!isMabimbsCompliant(latitude, longitude, elevation, newMoonDate)) {
-      return calculateNextHijriDate(newMoonDate, latitude, longitude, elevation, criteria)
+    while (true) {
+      newMoon = SearchMoonPhase(0, date, -30)
+      date = new AstroTime(newMoon.date)
+      dateInNewMoon = new Date(newMoon.date.getFullYear(), newMoon.date.getMonth(), newMoon.date.getDate())
+      newMoonDate = new AstroTime(dateInNewMoon)
+      sunset = SearchRiseSet(Body.Sun, observer, -1, newMoonDate, 1, elevation)
+      moonElongation = Elongation(Body.Moon, sunset)
+      moonEquator = Equator(Body.Moon, sunset, observer, true, true)
+      moonHorizon = Horizon(sunset, observer, moonEquator.ra, moonEquator.dec, 'normal')
+      if (moonElongation.elongation >= 6.4 && moonHorizon.altitude >= 3) {
+        console.log('Meet MABIMS Criteria:', `New Moon: ${newMoon.date}, Elongation: ${moonElongation.elongation}, Altitude: ${moonHorizon.altitude}`)
+        return newMoonDate
+      } else {
+        console.log('Doesn\'t meet MABIMS Criteria:', `New Moon: ${newMoon.date}, Elongation: ${moonElongation.elongation}, Altitude: ${moonHorizon.altitude}`)
+        return newMoonDate.AddDays(1)
+      }
     }
   } else {
-    if (!isKHGTCompliant(latitude, longitude, elevation, newMoonDate)) {
-      return calculateNextHijriDate(newMoonDate, latitude, longitude, elevation, criteria)
-    }
+    do {
+      newMoon = SearchMoonPhase(0, date, -30)
+      date = new AstroTime(newMoon.date)
+      dateInNewMoon = new Date(newMoon.date.getFullYear(), newMoon.date.getMonth(), newMoon.date.getDate())
+      newMoonDate = new AstroTime(dateInNewMoon)
+      sunset = SearchRiseSet(Body.Sun, observer, -1, newMoonDate, 1, elevation)
+      if (newMoon.date < sunset.date) {
+        console.log('Meet KHGT Criteria:', `New Moon: ${newMoon.date}, Sunset: ${sunset.date}`)
+        return newMoonDate
+      } else {
+        console.log('Doesn\'t meet KHGT Criteria:', `New Moon: ${newMoon.date}, Sunset: ${sunset.date}`)
+        return newMoonDate.AddDays(1)
+      }
+    } while (true)
   }
-  const daysSinceNewMoon = Math.floor((gregorianDate - newMoonDate) / 86400000)
-  const hijriYear = getHijriYear(newMoonDate)
-  const hijriMonth = getHijriMonth(daysSinceNewMoon)
-  const hijriDay = daysSinceNewMoon + 1
-  return { hijriYear, hijriMonth, hijriDay }
 }
 
-// function getHijriDate(gregorianDate, latitude, longitude, elevation, criteria) {
-//   const moonPhase = MoonPhase(gregorianDate)
-//   const elongationInfo = Elongation(Body.Moon, gregorianDate)
-//   const elongationDeg = elongationInfo.elongation
-//   const dateStart = new AstroTime(gregorianDate)
-//   const newMoonEvent = SearchMoonPhase(0, dateStart, 30)
-//   const newMoonTime = new AstroTime(newMoonEvent)
-//   const newMoonDate = newMoonEvent.date
-//   const observer = new Observer(latitude, longitude, elevation)
-//   const sunset = SearchRiseSet(Body.Sun, observer, -1, newMoonDate, 30, elevation)
-//   const sunsetTime = sunset.date
-//   let hijriMonthStart = newMoonDate
-//   if (criteria === '0') {
-//       const moonPosition = Equator(Body.Moon, sunset, observer, true, true)
-//       const horizonCoordinates = Horizon(sunsetTime, observer, moonPosition.ra, moonPosition.dec, 'normal')
-//       const moonAltitude = horizonCoordinates.altitude
-//       const moonElongation = Elongation(Body.Moon, sunsetTime)
-//       // MABIMS criteria: altitude >= 3 degrees and elongation >= 6.4 degrees
-//       if (moonAltitude < 3 && moonElongation < 6.4) {
-//           hijriMonthStart.setDate(hijriMonthStart.getDate() + 1)
-//       }
-//   } else if (criteria === '1') {
-//       // For KHGT, the new moon must occur before sunset on the same day
-//       if (newMoonDate > sunsetTime) {
-//           hijriMonthStart.setDate(hijriMonthStart.getDate() + 1)
-//       }
-//   }
-//   const daysSinceHijriStart = Math.floor((gregorianDate - hijriMonthStart))
-//   const hijriDay = daysSinceHijriStart + 1
-//   return { hijriMonthStart, hijriDay }
-// }
+const getCalendarData = (gregorianDate, latitude, longitude, elevation, criteria) => {
+  const newMoons = []
+  const gregorianFirstDate = new Date(gregorianDate.getFullYear(), 0, 1)
+  const startGregorianDate = new Date(`${gregorianDate.getFullYear()}-12-31T23:59:59Z`)
+  let startDate = new AstroTime(startGregorianDate)
+  let newMoonDate = gregorianFirstDate
+  console.log(newMoonDate.getFullYear(), gregorianFirstDate.getFullYear());
+  while (newMoonDate.getFullYear() === gregorianFirstDate.getFullYear()) {
+    newMoonDate = new Date(calculateNewMoon(startDate, latitude, longitude, elevation, criteria).date)
+    newMoons.push(newMoonDate)
+    startDate = new AstroTime(newMoonDate)
+    startDate = startDate.AddDays(-28)
+  }
+  const months = Array.from({ length: 12 }).map((_, monthIndex) => {
+    const firstDayOfMonth = new Date(gregorianDate.getFullYear(), monthIndex, 1).getDay()
+    const daysInMonth = new Date(gregorianDate.getFullYear(), monthIndex + 1, 0).getDate()
+    const daysArray = Array.from({ length: firstDayOfMonth }).fill(null)
+    for (let day = 1; day <= daysInMonth; day++) {
+      daysArray.push({
+        gregorian: day,
+        hijri: 0
+      })
+    }
+    return daysArray
+  })
+  newMoons.reverse().forEach((moonDate, index) => {
+    let hijriDayCounter = 1
+    let nextMoonIndex = 0
+    months.forEach((month, monthIdx) => {
+      month.forEach((dayObj, dayIdx) => {
+        if (dayObj !== null) {
+          const currentMoonDate = newMoons[nextMoonIndex]
+          const nextMoonDate = newMoons[nextMoonIndex + 1]
+          if (dayObj.gregorian === currentMoonDate.getDate() && monthIdx === currentMoonDate.getMonth()) {
+            hijriDayCounter = 1
+          }
+          dayObj.hijri = hijriDayCounter++
+          if (nextMoonDate && dayObj.gregorian === nextMoonDate.getDate() && monthIdx === nextMoonDate.getMonth()) {
+            hijriDayCounter = 1
+            nextMoonIndex++
+          }
+        }
+      })
+    })
+    if (moonDate.getFullYear() >= gregorianDate.getFullYear()) {
+      const lastYearDaysOffset = Math.floor((gregorianFirstDate - newMoons[0]) / (1000 * 3600 * 24))
+      hijriDayCounter = lastYearDaysOffset
+      const currentYearDaysOffset = Math.floor((newMoons[1] - gregorianFirstDate) / (1000 * 3600 * 24))
+      console.log(lastYearDaysOffset, currentYearDaysOffset);
+      for (let i = 0; i <= currentYearDaysOffset; i++) {
+        if (months[0][i] !== null) {
+          months[0][i].hijri = hijriDayCounter++
+        }
+      }
+      hijriDayCounter = 1
+    }
+  })
+  return months
+}
+
+const adjustedIslamicDate = (currentDate, latitude, longitude, elevation, criteria) => {
+  const currentTime = new AstroTime(currentDate)
+  const currentNewMoonTime = SearchMoonPhase(0, currentTime, 30)
+  const observer = new Observer(latitude, longitude, elevation)
+  const currentNewMoonDateTime = new Date(currentNewMoonTime.date.getFullYear(), currentNewMoonTime.date.getMonth(), currentNewMoonTime.date.getDate())
+  const sunset = SearchRiseSet(Body.Sun, observer, -1, currentNewMoonDateTime, 1, elevation)
+  const nextNewMoonTime = SearchMoonPhase(0, currentNewMoonTime, 30)
+  const daysBetweenNewMoons = (nextNewMoonTime.date - currentNewMoonTime.date) / 86400000
+  const moonEquator = Equator(Body.Moon, sunset, observer, true, true)
+  const moonHorizon = Horizon(sunset, observer, moonEquator.ra, moonEquator.dec, 'normal')
+  const elongation = AngleFromSun(Body.Moon, currentNewMoonTime)
+  let islamicMonthLength = 30
+  if (daysBetweenNewMoons < 30) islamicMonthLength = 29
+  let islamicDateOffset = 0
+  if (criteria === "0") {
+    if (moonHorizon.altitude >= 3 && elongation >= 6.4) islamicDateOffset = 0
+    else islamicDateOffset = 1
+  } else {
+    if (currentNewMoonTime.date < sunset.date) islamicDateOffset = 0
+    else islamicDateOffset = 1
+  }
+  const islamicDate = new Date()
+  islamicDate.setDate(currentDate.getDate() + islamicDateOffset)
+  const islamicDay = islamicDate.getDate()
+  if (islamicDay > islamicMonthLength) islamicDate.setDate(islamicDay - 1)
+  return islamicDate
+}
 
 const prayerTimesCorrection = () => [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 
-export { isStorageExist, pages, calculateHijriDate, prayerTimesCorrection }
+export { isStorageExist, pages, getCalendarData, adjustedIslamicDate, prayerTimesCorrection }
