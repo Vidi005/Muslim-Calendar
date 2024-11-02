e8f1fb6d4c8db7ab0935e58645cb7754d7e288d7
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-11-02 22:48:47 +0700

    feat: Add Custom Paging and Arrow Appends

---
+++ src/components/pages/home/CustomNextArrow.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const CustomNextArrow = props => {
+  const { className, style, onClick } = props
+  return (
+    <div
+      title="Next Month"
+      className={className}
+      style={{ ...style, display: "relative", height: "100%" }}
+      onClick={onClick}
+    >
+      <span className="absolute right-0 -left-1 inset-y-0 flex items-center justify-center h-full bg-gradient-to-l from-green-300 to-green-100 dark:from-gray-400 dark:to-gray-700 rounded-md duration-300">
+        <img className="object-contain object-center invert dark:invert-0 hover:brightness-75 duration-200" src="images/arrow-icon.svg" alt="Next" />
+      </span>
+    </div>
+  )
+}
+
+export default CustomNextArrow
