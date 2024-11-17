04c05ff3d69f7d6b01b3cddf9477e9c7433e4e62
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-11-17 16:00:57 +0700

    fix: Minor Bugs

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/',
   server: { port: 3000 },
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     legacy({ targets: ['defaults', 'not IE 11'] }),
     VitePWA({
