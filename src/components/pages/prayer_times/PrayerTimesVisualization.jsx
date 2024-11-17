249ee41195fa71add773e0fa6b5279b8d1904ce3
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-11-17 22:42:36 +0700

    feat: Add Select Calendar Type Option for Prayer Times

--- src/components/pages/prayer_times/PrayerTimesVisualization.jsx
+++ src/components/pages/prayer_times/PrayerTimesVisualization.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 
-const PrayerTimesVisualization = () => (
-  <section className="w-full md:w-1/2"></section>
+const PrayerTimesVisualization = ({ t }) => (
+  <section className="flex-1 w-full md:w-1/2 p-2 text-green-700 dark:text-gray-200 duration-200 animate__animated animate__fadeInUp">
+    <h2 className="text-center text-green-900 dark:text-white duration-200">{t('visualization')}</h2>
+  </section>
 )
 
 export default PrayerTimesVisualization
