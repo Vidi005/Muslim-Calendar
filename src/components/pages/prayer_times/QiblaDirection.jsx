de9fb108dd5cb3e6287c93a6c9e91d18da2a6c75
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-11-26 17:41:03 +0700

    fix: Overflowed Compass Background

--- src/components/pages/prayer_times/QiblaDirection.jsx
+++ src/components/pages/prayer_times/QiblaDirection.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const QiblaDirection = ({ t, heading, qiblaDirection }) => (
   <section className="flex-1 w-full md:w-1/2 p-2 md:px-4 2xl:px-8 text-green-700 dark:text-gray-200 duration-200 animate__animated animate__fadeInRight">
     <h2 className="text-center text-green-900 dark:text-white duration-200 whitespace-nowrap">{t('qibla_direction')}</h2>
-    <div className="qibla-container grid justify-center w-full p-4">
-      <div className="compass relative w-full max-w-xs md:max-w-sm rounded-full overflow-hidden duration-75" style={{ transform: `rotate(${heading === null ? 0 : heading}deg)` }}>
+    <div className="qibla-container grid justify-center w-full p-4 overflow-hidden">
+      <div className="compass relative w-full max-w-xs md:max-w-sm rounded-full duration-75 overflow-hidden" style={{ transform: `rotate(${heading === null ? 0 : heading}deg)` }}>
         <img className="w-full object-contain object-center dark:invert duration-200" src={`${import.meta.env.BASE_URL}images/compass-bg.png`} alt="Compass" />
         <img className="absolute origin-bottom top-0 h-1/2 w-full object-contain object-center duration-500" style={{ transform: `rotate(${qiblaDirection}deg)` }} src={`${import.meta.env.BASE_URL}images/direction-icon.svg`} alt="Direction" />
       </div>
