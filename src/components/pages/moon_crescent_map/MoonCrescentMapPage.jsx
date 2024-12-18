640ac0dfbfe2159e3e4d289e153dc35ce98a94d8
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-12-18 21:15:24 +0700

    fix: Moon Crescent Visibility Month Selection

--- src/components/pages/moon_crescent_map/MoonCrescentMapPage.jsx
+++ src/components/pages/moon_crescent_map/MoonCrescentMapPage.jsx
@@ -3,13 +3,18 @@ import { Helmet } from "react-helmet"
 import HeaderContainer from "../Header"
 import MainContainer from "./MainContainer"
 
-const MoonCrescentMapPage = ({ t, isSidebarExpanded}) => (
+const MoonCrescentMapPage = ({ t, isSidebarExpanded, formattedDateTime, hijriStartDates }) => (
   <div className="prayer-times-page h-screen w-full flex flex-col animate__animated animate__fadeIn">
     <Helmet>
       <title>{t('pages.2')}</title>
     </Helmet>
     <HeaderContainer />
-    <MainContainer t={t} isSidebarExpanded={isSidebarExpanded} />
+    <MainContainer
+      t={t}
+      isSidebarExpanded={isSidebarExpanded}
+      formattedDateTime={formattedDateTime}
+      hijriStartDates={hijriStartDates}
+    />
   </div>
 )
 
