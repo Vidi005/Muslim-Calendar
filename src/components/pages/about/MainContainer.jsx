f7a6d239762c9792444f8a80e36ab3afc41bd207
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-12-31 14:55:19 +0700

    feat: Add About Page Contents

---
+++ src/components/pages/about/MainContainer.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Sidebar from "../Sidebar"
+import AboutContent from "./AboutContent"
+import BottomBar from "../BottomBar"
+
+const MainContainer = ({ t, isSidebarExpanded }) => (
+  <main className="about-page-main h-0 flex-auto flex-nowrap bg-green-100 dark:bg-gray-800 duration-200">
+    {innerWidth > 1024
+      ? (
+        <div className="about-page-container flex flex-nowrap w-full h-full">
+          <Sidebar t={t} isSidebarExpanded={isSidebarExpanded}/>
+          <AboutContent t={t}/>
+        </div>
+        )
+      : (
+        <div className="about-page-container flex flex-col w-full h-full">
+          <AboutContent t={t}/>
+          <BottomBar t={t}/>
+        </div>
+        )
+    }
+  </main>
+)
+
+export default MainContainer
