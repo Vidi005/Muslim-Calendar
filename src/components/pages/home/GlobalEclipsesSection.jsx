ed1d2fbd2ad464492919e917f40d4d29eac294d6
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-01-02 21:50:01 +0700

    fix: Moon Parallactic Angle Formula

---
+++ src/components/pages/home/GlobalEclipsesSection.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const GlobalEclipsesSection = () => (
+  <section className="global-eclipses-section flex flex-col items-center w-full px-2 md:px-4 text-green-700 dark:text-gray-200 duration-200 animate__animated animate__fadeInUp">
+    <h1 className="m-4 text-center text-green-900 dark:text-white duration-200">Global Eclipses</h1>
+  </section>
+)
+
+export default GlobalEclipsesSection
