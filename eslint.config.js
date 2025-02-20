dc2fb18ce04bbadc8387ec74302bc0eeff095486
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-02-20 16:22:57 +0700

    refactor: Remove Unused Parameter

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,24 @@ import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    parser: "espree",
+    rules: {
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'off',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+      'no-unused-vars': 'off'
+    }
+  },
 ];
