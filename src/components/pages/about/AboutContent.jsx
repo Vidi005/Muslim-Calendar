09c53b107b3c357273bc2af50c8d93438a05103b
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-05-03 14:04:28 +0700

    perf: Improve Web App UI/UX

--- src/components/pages/about/AboutContent.jsx
+++ src/components/pages/about/AboutContent.jsx
@@ -11,18 +11,18 @@ const AboutContent = ({ t }) => (
     <section className="my-4 text-left">
       <h3 className="text-green-700 dark:text-gray-50">{t('resources')}</h3>
       <hr className="border border-green-700 dark:border-white" />
-      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0.5 md:gap-1 list-inside list-disc text-sm lg:text-base 2xl:text-lg text-blue-500 dark:text-blue-400 underline">
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0.5 md:gap-1 list-inside list-disc text-sm lg:text-base 2xl:text-lg text-blue-600 dark:text-blue-400 underline">
         {en.resource_items.map((_item, index) => (
-          <li key={index} className="w-full sm:w-auto p-1 hover:text-blue-700 dark:hover:text-blue-300 active:text-purple-700 dark:active:text-purple-500"><a href={t(`resource_items.${index}.url`)} target="_blank" rel="noopener noreferrer">{t(`resource_items.${index}.name`)}</a></li>
+          <li key={index} className="w-full sm:w-auto p-1 hover:text-blue-800 dark:hover:text-blue-300 active:text-purple-700 dark:active:text-purple-500"><a href={t(`resource_items.${index}.url`)} target="_blank" rel="noopener noreferrer">{t(`resource_items.${index}.name`)}</a></li>
         ))}
       </ul>
     </section>
     <section className="my-4 text-left">
       <h3 className="text-green-700 dark:text-gray-50">{t('references')}</h3>
       <hr className="border border-green-700 dark:border-white" />
-      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0.5 md:gap-1 list-inside list-disc text-sm lg:text-base 2xl:text-lg text-blue-500 dark:text-blue-400 underline">
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0.5 md:gap-1 list-inside list-disc text-sm lg:text-base 2xl:text-lg text-blue-600 dark:text-blue-400 underline">
         {en.reference_items.map((_item, index) => (
-          <li key={index} className="w-full sm:w-auto p-1 hover:text-blue-700 dark:hover:text-blue-300 active:text-purple-700 dark:active:text-purple-500"><a href={t(`reference_items.${index}.url`)} target="_blank" rel="noopener noreferrer">{t(`reference_items.${index}.name`)}</a></li>
+          <li key={index} className="w-full sm:w-auto p-1 hover:text-blue-800 dark:hover:text-blue-300 active:text-purple-700 dark:active:text-purple-500"><a href={t(`reference_items.${index}.url`)} target="_blank" rel="noopener noreferrer">{t(`reference_items.${index}.name`)}</a></li>
         ))}
       </ul>
     </section>
