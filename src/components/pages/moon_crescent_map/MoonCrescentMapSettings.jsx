dfa5ab032096aa852897b8404af5559e2b82cc26
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-06-07 11:37:01 +0700

    fix: Moon Crescent Visibility Map Calculation

--- src/components/pages/moon_crescent_map/MoonCrescentMapSettings.jsx
+++ src/components/pages/moon_crescent_map/MoonCrescentMapSettings.jsx
@@ -80,8 +80,8 @@ const MoonCrescentMapSettings = ({ selectedHijriMonth, selectHijriMonth, areMoon
                       <label htmlFor="elongation-type">{t('moon_infos.11').split(' ')[0]}:</label>
                       <select
                         className="ml-1 p-1 bg-green-200 dark:bg-gray-200 rounded shadow-inner duration-200 cursor-not-allowed"
-                        defaultValue={1}
-                        value={1}
+                        defaultValue={state.selectedMoonVisibilityCriteria === 6 ? 0 : 1}
+                        value={state.selectedMoonVisibilityCriteria === 6 ? 0 : 1}
                         disabled
                       >
                         {en.elongation_type.map((type, index) => <option key={type} value={index}>{t(`elongation_type.${index}`)}</option>)}
