93e7a8a8356c183261bd113202555b8a9ac846c0
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-06-07 20:25:57 +0700

    feat: Add New Moon Crescent Visibility Infos Tooltip on Cursor Hover

--- src/components/pages/home/MoonCrescentMapSection.jsx
+++ src/components/pages/home/MoonCrescentMapSection.jsx
@@ -37,7 +37,7 @@ const MoonCrescentMapSection = () => (
                   </>
                 ))}
                 {state.moonCrescentVisibility.zoneCoordinates?.map((marker, index) => (
-                  <span key={index} className="absolute opacity-50 -translate-y-1/2" style={{
+                  <span title={marker.tooltip} key={index} className="absolute opacity-50 -translate-y-1/2" style={{
                     width: `${marker.width}%`,
                     height: `${marker.height}%`,
                     backgroundColor: marker.color,
