e25545decc42d0c2ed4db4c8e2762766ee506811
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-06-29 10:13:07 +0700

    refactor: Change Comments

--- src/utils/data.js
+++ src/utils/data.js
@@ -248,7 +248,7 @@ const calculateNewMoon = (prevNewMoonDate, startDate, timeZone, latitude, longit
       fajrAtWellington = SearchAltitude(Body.Sun, observerFromNewZealand, +1, newMoon, 2, -18)
       isConjunctionBeforeFajr = (fajrAtWellington.date.getUTCDate() === newMoon.date.getUTCDate() && fajrAtWellington.date > newMoon.date)
       if (isMetCriteria && isConjunctionBeforeFajr) {
-        // Met the Unified Global Hijri Calendar criteria (Met the Visibility Criteria before 00:00 UTC on any location or Met the Visibility Criteria after 00:00 UTC in any America continents plains and Conjunction before Fajr in New Zealand on Ijtima' Day)
+        // Met the Unified Global Hijri Calendar criteria
         return newMoonDate.AddDays(1)
       } else {
         // Didn't meet the Unified Global Hijri Calendar criteria
