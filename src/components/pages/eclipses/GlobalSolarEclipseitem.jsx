530a23b93237bd657b526d15329ae137613c97fb
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2025-07-12 23:17:15 +0700

    fix: Restore Deleted Global Solar Eclipse Component

---
+++ src/components/pages/eclipses/GlobalSolarEclipseitem.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import en from "../../../locales/en.json"
+
+const GlobalSolarEclipseItem = ({ t, selectedLanguage, selectedTimeZone, globalSolarEclipse }) => {
+  let globalSolarEclipseDate = ''
+  let solarEclipsePeakTime = ''
+  try {
+    globalSolarEclipseDate = globalSolarEclipse?.peak?.toLocaleDateString(selectedLanguage || 'en', { calendar: 'gregory', weekday: 'long', day: '2-digit', month: 'long', year: 'numeric', timeZone: selectedTimeZone }).replace(/Minggu/g, 'Ahad').replace(/Jumat/g, 'Jum\'at')
+    solarEclipsePeakTime = `${globalSolarEclipse?.peak?.toLocaleTimeString(selectedLanguage || 'en', { timeZone: selectedTimeZone, timeZoneName: 'short', hour12: false, hourCycle: 'h23', hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(/\./g, ':')}`
+  } catch (error) {
+    globalSolarEclipseDate = t('unsupported_time_format')
+    solarEclipsePeakTime = t('unsupported_time_format')
+  }
+  const solarEclipseKind = globalSolarEclipse?.kind?.charAt(0)?.toUpperCase() + globalSolarEclipse?.kind?.slice(1)
+  const distance = `${globalSolarEclipse?.distance?.toFixed(6)} km`
+  const latitude = `${globalSolarEclipse?.kind === 'partial' ? t(`global_solar_eclipse_infos.${en.global_solar_eclipse_infos.length - 1}`) : globalSolarEclipse?.latitude + '°'}`
+  const longitude = `${globalSolarEclipse?.kind === 'partial' ? t(`global_solar_eclipse_infos.${en.global_solar_eclipse_infos.length - 1}`) : globalSolarEclipse?.longitude + '°'}`
+  const solarEclipseObscuration = `${globalSolarEclipse?.kind === 'partial' ? t(`global_solar_eclipse_infos.${en.global_solar_eclipse_infos.length - 1}`) : (globalSolarEclipse?.obscuration * 100).toFixed(2) + '%'}`
+  const globalSolarEclipseArrayData = [
+    solarEclipseKind,
+    distance,
+    latitude,
+    longitude,
+    solarEclipseObscuration,
+    solarEclipsePeakTime
+  ]
+  return (
+    <div className="w-full bg-green-50 dark:bg-black p-2 md:px-4 text-base md:text-sm lg:text-base rounded-md md:rounded-lg shadow lg:shadow-md dark:shadow-white/50 overflow-hidden duration-200">
+      <h4 className="mb-1 text-center text-base md:text-sm lg:text-base text-green-900 dark:text-white font-bold">{globalSolarEclipseDate}</h4>
+      <table className="table-auto">
+        {en.global_solar_eclipse_infos.slice(0, -1).map((_, index) => (
+          <tr className="align-top text-black dark:text-gray-100" key={index}>
+            <td className="whitespace-nowrap">{t(`global_solar_eclipse_infos.${index}`)}</td>
+            <td>&nbsp;:&nbsp;</td>
+            <td>{globalSolarEclipseArrayData[index]}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
+
+export default GlobalSolarEclipseItem
